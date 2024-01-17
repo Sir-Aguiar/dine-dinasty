6e3f2da8e1139633eae540798dbdc3489e4619a8
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-01-17 16:13:43 -0400

    feat: Post creation base business logic

--- backend/src/repositories/Post/PostCreationRepository.ts
+++ backend/src/repositories/Post/PostCreationRepository.ts
@@ -1,7 +1,8 @@
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-import { prisma } from "../database/client";
-import { IPost } from "../entities/Post";
-import { CreatePostInput } from "../use-cases/Post/create-post";
+import { prisma } from "../../database/client";
+import { IPost } from "../../entities/Post";
+import { CreatePostInput } from "../../use-cases/Post/create-post";
+import { ServerError } from "../../entities/ServerError";
 
 export interface IPostCreationRepository {
   execute({ threadId, userId, title, ownerReview }: CreatePostInput): Promise<IPost>;
@@ -14,11 +15,10 @@ export class PostCreationRepository implements IPostCreationRepository {
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === "P2003") {
-          
+          if (error.meta?.field_name === "userId") throw new ServerError(403, "Nenhum usuário foi identificado");
+          if (error.meta?.field_name === "threadId") throw new ServerError(403, "Nenhuma conversa foi identificada");
         }
       }
-
-
       throw error;
     }
   }
--- backend/src/use-cases/Post/create-post.ts
+++ backend/src/use-cases/Post/create-post.ts
@@ -1,5 +1,6 @@
+import { EntityError } from "../../entities/EntityError";
 import { IPost } from "../../entities/Post";
-import { IPostCreationRepository } from "../../repositories/PostCreationRepository";
+import { IPostCreationRepository } from "../../repositories/Post/PostCreationRepository";
 
 export interface CreatePostInput {
   threadId: string;
@@ -13,8 +14,16 @@ export type CreatePostOutput = Promise<IPost>;
 export class CreatePost {
   constructor(private postRepository: IPostCreationRepository) {}
   async execute({ threadId, title, userId, ownerReview }: CreatePostInput): CreatePostOutput {
+    if (typeof threadId !== "string") throw new EntityError("Identificador de conversa em formato inválido");
 
-    
+    if (typeof userId !== "number") throw new EntityError("Identificador usuário em formato inválido");
+
+    if (typeof title !== "string") throw new EntityError("Título em formato inválido");
+
+    if (ownerReview && typeof ownerReview !== "string") throw new EntityError("Descrição está em formato inválido");
+
+    if (title.length > 150) throw new EntityError("O título deste post deve ter no máximo 150 letras");
+
     return await this.postRepository.execute({ threadId, title, userId, ownerReview });
   }
 }
